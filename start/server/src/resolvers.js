f1221a57dba5fcd249427a04af9d5d0a95f88d76
Isaac Tait <isaac@mountaintopcoding.com>
2020-03-28 21:26:23 -0700

    write resolver on user and mission types

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -41,3 +41,28 @@ module.exports = {
     },
   },
 }
+
+module.exports = {
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+}
+
+module.exports = {
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+  
+      if (!launchIds.length) return [];
+  
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
+}
