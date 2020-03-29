e8a9b82283a466a2bf172c0af6dd2a405f93bb10
Isaac Tait <isaac@mountaintopcoding.com>
2020-03-28 20:53:01 -0700

    something got broken

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,9 +1,23 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs, //typeDefs is a keyword found in both C & C++... am I writing C/C++ right now?
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-  
+  console.log(`🚀 Server ready at ${url}`);
+});
