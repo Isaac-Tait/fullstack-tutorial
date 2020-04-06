ee501158c0947b18873961709e3e99d6968be32c
Isaac Tait <isaac@mountaintopcoding.com>
2020-04-06 06:59:55 -0700

    all good things must come to an end

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import gql from 'graphql-tag'; 
 
+import { Loading, Header, LaunchDetail } from '../components';
+import { ActionButton } from '../containers';
+import { RouteComponentProps } from '@reach/router';
+import * as LaunchDetailsTypes from './__generated__/LaunchDetails';
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
 interface LaunchProps extends RouteComponentProps {
 
 }
